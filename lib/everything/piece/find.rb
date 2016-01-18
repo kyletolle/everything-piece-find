48bfb32614cebed32c326e9e0053064a913590fe
Kyle Tolle <kyle@nullsix.com>
2016-01-17 23:29:41 -0700

    Initial commit

---
+++ lib/everything/piece/find.rb
@@ -0,0 +1,9 @@
+require "everything/piece/find/version"
+
+module Everything
+  module Piece
+    module Find
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/everything/piece/find/version.rb
@@ -0,0 +1,7 @@
+module Everything
+  module Piece
+    module Find
+      VERSION = "0.1.0"
+    end
+  end
+end
