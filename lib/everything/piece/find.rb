e965cd49be98e35d5848bf37baee48095a1235c8
Kyle Tolle <kyle@nullsix.com>
2016-01-20 22:33:13 -0700

    Add #find_by_name_recursive to Find module

--- lib/everything/piece/find.rb
+++ lib/everything/piece/find.rb
@@ -17,6 +17,31 @@ module Everything
           raise ArgumentError, error_message
         end
       end
+
+      def find_by_name_recursive(piece_name)
+        glob_path = File.join(Everything.path, '**', piece_name)
+        possible_dirs = Dir.glob(glob_path)
+        full_piece_path = possible_dirs.first
+
+        raise_no_piece_error(piece_name) unless full_piece_path
+        raise_not_directory_error(piece_name, full_piece_path) unless Dir.exist?(full_piece_path)
+
+        Piece.new(full_piece_path)
+      end
+
+    private
+
+      def raise_no_piece_error(piece_name)
+        error_message =
+          %Q{No piece "#{piece_name}" found in "#{Everything.path}" or subdirectories}
+        raise ArgumentError, error_message
+      end
+
+      def raise_not_directory_error(piece_name, piece_path)
+        error_message =
+          %Q{Found file "#{piece_name}" at "#{piece_path}", but was expecting a directory}
+        raise ArgumentError, error_message
+      end
     end
   end
 end
--- spec/everything/piece/find_spec.rb
+++ spec/everything/piece/find_spec.rb
@@ -25,16 +25,13 @@ describe Everything::Piece::Find do
   context '#find_by_name' do
     include_context 'with stubbed everything path'
 
-    let(:given_piece_name) do
-      'seconds-away-from-you'
-    end
-
     context 'where the directory does not exist' do
+      let(:given_piece_name) { 'wont-exist' }
       let(:expected_error_message) do
         %Q{No piece "#{given_piece_name}" found in "#{Everything.path}"}
       end
 
-      it 'raises an ArgumentError error' do
+      it 'raises an ArgumentError' do
         expect { finder.find_by_name(given_piece_name) }
           .to raise_error(ArgumentError, expected_error_message)
       end
@@ -67,6 +64,127 @@ describe Everything::Piece::Find do
         expect(actual_piece.full_path).to eq(expected_piece_path)
       end
     end
+  end
 
+  describe '#find_by_name_recursive' do
+    include_context 'with stubbed everything path'
+
+    context 'where the directory does not exist' do
+      let(:given_piece_name) { 'wont-exist' }
+      let(:expected_error_message) do
+        %Q{No piece "#{given_piece_name}" found in "#{Everything.path}" or subdirectories}
+      end
+
+      it 'raises an ArgumentError' do
+        expect { finder.find_by_name_recursive(given_piece_name) }
+          .to raise_error(ArgumentError, expected_error_message)
+      end
+    end
+
+    context 'where a file with that name exists, but it is not a directory' do
+      let(:tmp_sub_dir) do
+        Dir.mktmpdir
+      end
+      let(:given_piece_name) do
+        File.basename tmp_piece_path
+      end
+      let!(:tmp_piece_path) do
+        Tempfile.new('just-a-file-under-sub-dir', tmp_sub_dir).path
+      end
+      let(:expected_error_message) do
+        %Q{Found file "#{given_piece_name}" at "#{tmp_piece_path}", but was expecting a directory}
+      end
+
+      after do
+        FileUtils.remove_entry tmp_sub_dir
+      end
+
+      it 'returns an ArgumentError' do
+        expect { finder.find_by_name_recursive(given_piece_name) }
+          .to raise_error(ArgumentError, expected_error_message)
+      end
+    end
+
+    context 'where one directory with that name exists' do
+      let(:tmp_sub_dir) do
+        Dir.mktmpdir
+      end
+      let(:given_piece_name) do
+        'piece-under-sub-dir'
+      end
+      let(:tmp_piece_path) do
+        File.join(tmp_sub_dir, given_piece_name)
+      end
+
+      before do
+        tmp_sub_dir
+        Dir.mkdir(tmp_piece_path)
+      end
+
+      after do
+        FileUtils.remove_entry tmp_sub_dir
+      end
+
+      let(:actual_piece) do
+        finder.find_by_name_recursive(given_piece_name)
+      end
+      let(:expected_piece_path) do
+        tmp_piece_path
+      end
+
+      it 'returns a piece' do
+        expect(actual_piece).to be_a(Everything::Piece)
+      end
+
+      it 'returns the piece from the sub directory' do
+        expect(actual_piece.full_path).to eq(expected_piece_path)
+      end
+    end
+
+    context 'where multiple directories of the same name exist' do
+      let(:first_tmp_sub_dir) do
+        Dir.mktmpdir
+      end
+      let(:given_piece_name) do
+        'piece-under-sub-dir'
+      end
+      let(:first_tmp_piece_path) do
+        File.join(first_tmp_sub_dir, given_piece_name)
+      end
+      let(:second_tmp_sub_dir) do
+        Dir.mktmpdir
+      end
+      let(:second_tmp_piece_path) do
+        File.join(second_tmp_sub_dir, given_piece_name)
+      end
+
+      before do
+        first_tmp_sub_dir
+        Dir.mkdir(first_tmp_piece_path)
+
+        second_tmp_sub_dir
+        Dir.mkdir(second_tmp_piece_path)
+      end
+
+      after do
+        FileUtils.remove_entry first_tmp_sub_dir
+        FileUtils.remove_entry second_tmp_sub_dir
+      end
+
+      let(:actual_piece) do
+        finder.find_by_name_recursive(given_piece_name)
+      end
+      let(:expected_piece_path) do
+        first_tmp_piece_path
+      end
+
+      it 'returns a piece' do
+        expect(actual_piece).to be_a(Everything::Piece)
+      end
+
+      it 'returns the piece from the first subdirectory' do
+        expect(actual_piece.full_path).to eq(expected_piece_path)
+      end
+    end
   end
 end
